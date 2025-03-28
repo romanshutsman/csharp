e5c7b8cc71a035f788aa102386f6911c8b4729b6
shutsmanroman <roman13275@gmail.com>
2025-03-28 14:03:37 +0100

    add post controller

---
+++ api/Dtos/PostToAddDto.cs
@@ -0,0 +1,20 @@
+namespace api.DTO
+{
+    public partial class PostToAddDto
+    {
+        public string PostTitle {get; set;}
+        public string PostContent {get; set;}
+
+        public PostToAddDto()
+        {
+            if (PostTitle == null)
+            {
+                PostTitle = "";
+            }
+            if (PostContent == null)
+            {
+                PostContent = "";
+            }
+        }
+    }
+}
